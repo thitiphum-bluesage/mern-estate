2dac82b96cec5e2971c45ac923d5a0f94f45c182
Thitiphum Chaikarnjanakit <thitiphum.dev@gmail.com>
2024-01-01 20:01:27 +0700

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSehema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSehema);
+
+export default User;
